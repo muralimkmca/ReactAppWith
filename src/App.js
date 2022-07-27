f2d0571740424b1b27a61ac9fa633ed3f39dce1d
Murali <muralimk@outlook.com>
2022-07-27 15:39:51 +0530

    reactapplication with route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Home } from './component/Home';
+import { Department } from './component/Department';
+import { Employee } from './component/Employee';
+import { Navigation} from './component/Navigation';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (   
+    <div>
+      <Navigation />
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} exact />
+            <Route path="/dept" element={<Department/>} />
+            <Route path="/emp" element={<Employee/>} />
+          </Routes>      
+    
+          </BrowserRouter>
+          </div>
   );
 }
 
